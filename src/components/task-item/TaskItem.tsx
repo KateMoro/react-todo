c49c97aacaecc3470386b9ecca962c4c0ab17d4a
Katerina <kathty@gmail.com>
2023-01-29 19:30:35 +0300

    Add task editing

--- src/components/task-item/TaskItem.tsx
+++ src/components/task-item/TaskItem.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react';
 import { useToDoStore } from '../../data/stores/useToDoStore';
 import styles from './TaskItem.module.scss';
 
@@ -7,14 +8,58 @@ interface TaskItem {
 }
 
 const TaskItem: React.FC<TaskItem> = ({ id, title }) => {
-  const { removeTask } = useToDoStore();
+  const { removeTask, updateTask } = useToDoStore();
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [value, setValue] = useState(title);
+  const taskEditInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (isEditMode) {
+      taskEditInputRef?.current?.focus();
+    }
+  }, [isEditMode]);
 
   return (
     <li className={styles.task}>
-      <label>
+      <label className={styles.taskLabel}>
         <input className={styles.taskCheckbox} type="checkbox" />
-        <span>{title}</span>
+        {isEditMode ? (
+          <input
+            className={styles.taskEditInput}
+            type="text"
+            ref={taskEditInputRef}
+            value={value}
+            onChange={(evt) => setValue(evt.target.value)}
+            onKeyDown={(evt) => {
+              if (evt.key === 'Enter') {
+                updateTask(id, value);
+                setIsEditMode(false);
+              }
+            }}
+          />
+        ) : (
+          <span>{title}</span>
+        )}
       </label>
+
+      {isEditMode ? (
+        <button
+          className={`${styles.taskButton} ${styles.taskButtonSave}`}
+          type="button"
+          aria-label="save"
+          onClick={() => {
+            updateTask(id, value);
+            setIsEditMode(false);
+          }}
+        />
+      ) : (
+        <button
+          className={`${styles.taskButton} ${styles.taskButtonEdit}`}
+          type="button"
+          aria-label="edit"
+          onClick={() => setIsEditMode(true)}
+        />
+      )}
 
       <button
         className={`${styles.taskButton} ${styles.taskButtonRemove}`}
